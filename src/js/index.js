29d26faced5b33b00bd36fe6b271d45368a77455
Alexandre Alves <101531940+XandAvS@users.noreply.github.com>
2024-04-07 23:27:35 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,55 @@
+/*
+    Objetivo 1 - quando o usuário clicar no botão de mostrar mais deve abrir os projetos que estão escondidos no html
+
+        Passo 1 - pegar o botão mostrar mais no JS pra poder verificar quando o usuário clicar em cima dele
+
+        Passo 2 - identificar o clique no botão
+        
+        Passo 3 - adicionar a classe "ativo" nos projetos escondidos
+
+    Objetivo 2 - esconder o botão de mostrar mais
+        Passo 1 - pegar o botão e esconder ele
+*/
+
+// Objetivo 1 - quando o usuário clicar no botão de mostrar mais deve abrir os projetos que estão escondidos no html
+
+// Passo 1 - pegar o botão mostrar mais no JS pra poder verificar quando o usuário clicar em cima dele
+const botaoMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
+const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)');
+
+botaoMostrarProjetos.addEventListener('click', () => {
+    if (botaoMostrarProjetos.classList.contains('remover')) {
+        esconderMaisProjetos();
+        botaoMostrarProjetos.classList.remove('remover');
+        // Trocar o texto do botão para "MOSTRAR MAIS"
+        botaoMostrarProjetos.textContent = 'Mostrar mais';
+    } else {
+        mostrarMaisProjetos();
+        esconderBotao();
+        // Trocar o texto do botão para "MOSTRAR MENOS"
+        botaoMostrarProjetos.textContent = 'Mostrar menos';
+    }
+    // Passo 3 - adicionar a classe "ativo" nos projetos escondidos
+    //mostrarMaisProjetos();
+
+    // Objetivo 2 - esconder o botão de mostrar mais
+    // Passo 1 - pegar o botão e esconder ele
+    //esconderBotao();
+});
+
+function esconderBotao() {
+    botaoMostrarProjetos.classList.add('remover');
+}
+
+function mostrarMaisProjetos() {
+    projetosInativos.forEach(projetoInativo => {
+        projetoInativo.classList.add('ativo');
+    });
+}
+function esconderMaisProjetos() {
+    projetosInativos.forEach(projetoInativo => {
+        projetoInativo.classList.remove('ativo');
+    });
+}
+
+
